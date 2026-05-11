01031956101584faa764ea5860f6c51288269bff
serrrfirat <f@nuff.tech>
2026-05-11 20:24:05 +0200

    Use match for Reborn completion shell handling

--- crates/ironclaw_reborn_cli/src/commands/completion.rs
+++ crates/ironclaw_reborn_cli/src/commands/completion.rs
@@ -15,16 +15,19 @@ impl CompletionCommand {
         let mut command = crate::cli::command();
         let bin_name = command.get_name().to_string();
 
-        if self.shell == Shell::Zsh {
-            let mut buffer = Vec::new();
-            generate(self.shell, &mut command, bin_name.clone(), &mut buffer);
-            let script = String::from_utf8(buffer)?;
+        match self.shell {
+            Shell::Zsh => {
+                let mut buffer = Vec::new();
+                generate(self.shell, &mut command, bin_name.clone(), &mut buffer);
+                let script = String::from_utf8(buffer)?;
 
-            let bare = format!("compdef _{0} {0}", bin_name);
-            let guarded = format!("(( $+functions[compdef] )) && compdef _{0} {0}", bin_name);
-            io::stdout().write_all(script.replace(&bare, &guarded).as_bytes())?;
-        } else {
-            generate(self.shell, &mut command, bin_name, &mut io::stdout());
+                let bare = format!("compdef _{0} {0}", bin_name);
+                let guarded = format!("(( $+functions[compdef] )) && compdef _{0} {0}", bin_name);
+                io::stdout().write_all(script.replace(&bare, &guarded).as_bytes())?;
+            }
+            shell => {
+                generate(shell, &mut command, bin_name, &mut io::stdout());
+            }
         }
 
         Ok(())
